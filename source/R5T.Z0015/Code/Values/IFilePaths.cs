cf8a4259295e21763df583f1618c506a8b8a34d4
davidcoats <david@getsafetycone.com>
2022-10-26 04:20:10 -0700

    Interim changes.

--- source/R5T.Z0015/Code/Values/IFilePaths.cs
+++ source/R5T.Z0015/Code/Values/IFilePaths.cs
@@ -8,6 +8,7 @@ namespace R5T.Z0015
 	[ValuesMarker]
 	public partial interface IFilePaths : IValuesMarker
 	{
+		public string OutputJsonFilePath => @"C:\Temp\Output.json";
 		public string OutputTextFilePath => @"C:\Temp\Output.txt";
 		public string ResultOutputJsonFilePath => @"C:\Temp\Result.json";
 	}
