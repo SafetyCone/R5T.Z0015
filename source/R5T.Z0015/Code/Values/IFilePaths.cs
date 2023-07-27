8ce9a34e465970f5b5f36f45b02d5071f283a5df
davidcoats <david@getsafetycone.com>
2023-07-27 01:32:37 -0700

    Interim changes.

--- source/R5T.Z0015/Code/Values/IFilePaths.cs
+++ source/R5T.Z0015/Code/Values/IFilePaths.cs
@@ -10,9 +10,13 @@ namespace R5T.Z0015
 	{
 		public string OutputErrorsTextFilePath => @"C:\Temp\Errors.txt";
 
+		public string HumanOutputTextFilePath => @"C:\Temp\Human Output.txt";
+		public string LogFilePath => @"C:\Temp\log.txt";
+		public string OutputDataFilePath => @"C:\Temp\Output.dat";
         public string OutputHtmlFilePath => @"C:\Temp\Output.html";
         public string OutputJsonFilePath => @"C:\Temp\Output.json";
 		public string OutputTextFilePath => @"C:\Temp\Output.txt";
+        public string OutputTextFilePath_Secondary => @"C:\Temp\Output2.txt";
         public string OutputXmlFilePath => @"C:\Temp\Output.xml";
         public string RawOutputJsonFilePath => @"C:\Temp\Output-Raw.json";
 		public string ResultOutputJsonFilePath => @"C:\Temp\Result.json";
